0e381a85399e287c26bca9fee3960cbddc325388
Trung1234 <trungsieunhan24@gmail.com>
2019-12-07 17:04:32 +0700

    fix menu

--- js/newjavascrip.js
+++ js/newjavascrip.js
@@ -17,6 +17,21 @@ $("#sub").click(function(){
 });
 
 $(document).ready(function(){
+    $(".vg-b1").click(function(){
+        $("i").hide()
+        var ten = $(".vg-i1").val();
+        var mail = $(".vg-i2").val();
+        var subject = $(".vg-i3").val();
+        if(ten.length < 2){
+            $(".bloi1").show();
+        }
+        else if(mail == ""){
+            $(".bloi2").show();
+        }
+        else if(subject == ""){
+            $(".bloi3").show();
+        }
+       })
     // Add minus icon for collapse element which is open by default
     $(".collapse.show").each(function(){
         $(this).prev(".card-header").find(".fa").addClass("fa-minus").removeClass("fa-plus");
